7a003c460db11d94f1055e3c82c5c9483300f276
David Ma <183250896+MadAvidCoder@users.noreply.github.com>
2025-12-16 22:09:09 +1100

    Fix typo
    Fixed a minor typo

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ app.get('/', (c) => {
     return c.text(
 `North Pole Official API Manual: Santa's Naughty & Nice Tracker - Elf Edition
 
-WARNING: This manual is the property of S. GIFT INC. (Santa's Grand Institute of Festive Transparency Incorporated). For internal use only. Any elves found to be endpoints above their clearance level (especially those marked SACK or SUPREME) will face punitive action, including untangling Christmas lights in sub-zero temperatures. Please read carefully.
+WARNING: This manual is the property of S. GIFT INC. (Santa's Grand Institute of Festive Transparency Incorporated). For internal use only. Any elves found to be usnig endpoints above their clearance level (especially those marked SACK or SUPREME) will face punitive action, including untangling Christmas lights in sub-zero temperatures. Please read carefully.
 
 ------------------------------
 
